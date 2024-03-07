e1d04572de5fecbed1cfb67836188d7e7ef75112
Matejicek <xzalma00@vutbr.cz>
2024-03-07 09:23:31 +0100

    implemented clasyfing, but updated >= to <=, be aware of this

--- project-loadingTree.hs
+++ project-loadingTree.hs
@@ -1,4 +1,7 @@
 
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
 data DecisionTree attributeIndex attributeThreshold className 
     = EmptyTree
     | Node attributeIndex attributeThreshold (DecisionTree attributeIndex attributeThreshold className ) (DecisionTree attributeIndex attributeThreshold className )
@@ -113,7 +116,7 @@ decisionTree_clasify :: DecisionTree Int Double String -> [Double] -> String
 decisionTree_clasify (Leaf className) arr = className
 decisionTree_clasify EmptyTree arr = ""
 decisionTree_clasify (Node aI aT (left) (right)) arr 
-    | (arr !! aI) >= aT = decisionTree_clasify right arr
+    | (arr !! aI) > aT = decisionTree_clasify right arr
     | otherwise = decisionTree_clasify left arr
 
 
@@ -192,13 +195,13 @@ splitData :: [([Double], String)] -> Int -> (Int, Double) -> ([([Double], String
 splitData inputData processingIndex (aI, aT)
     | length(inputData) == (processingIndex + 1) = do 
         let (rowData, className) = inputData !! processingIndex
-        if ((rowData !! aI) < aT)
+        if ((rowData !! aI) <= aT)
             then ([inputData !! processingIndex], [])
             else ([], [inputData !! processingIndex])
     | otherwise = do 
         let (leftData, rightData) = splitData inputData (processingIndex + 1) (aI, aT)
         let (rowData, className) = inputData !! processingIndex
-        if ((rowData !! aI) < aT)
+        if ((rowData !! aI) <= aT)
             then (inputData !! processingIndex : leftData, rightData)
             else (leftData, inputData !! processingIndex : rightData)
 
@@ -242,7 +245,6 @@ initBuckets inputData processingIndex
     if(newClassesLen == oldClassesLen)
         then do 
             let classIndex = findIndexOf newClasses className
-            -- let leftBucketNewValue = (leftBucketNew !! classIndex + 1)
             let rightBucketNewValue = (rightBucket !! classIndex + 1)
             let rightBucketNew = setValueAt rightBucket rightBucketNewValue classIndex
             (leftBucket, rightBucketNew, newClasses)
@@ -252,10 +254,95 @@ initBuckets inputData processingIndex
             (leftBucketNew, rightBucketNew, newClasses)
     
 
-learnDecisionTree :: [([Double], String)] -> Int -> DecisionTree Int Double String 
-learnDecisionTree inputData processingIndex = do 
+
+giniScore :: [Int] -> Int -> Double
+giniScore bucket classID = do 
+    let sumInBucket = fromIntegral (sum bucket) :: Double
+    let classCount = fromIntegral (bucket !! classID) :: Double
+    1.0 - ((classCount / sumInBucket) ^ 2)
+
+calcGini :: ([Int], Double) -> ([Int], Double) -> Int -> Double
+calcGini (leftBucket, giniLeft) (rightBucket, giniRight) inputDataLen = do 
+    let inputDataLenDouble = fromIntegral inputDataLen :: Double
+    let sumLeftBucketDouble = fromIntegral (sum leftBucket) :: Double
+    let sumRightBucketDouble = fromIntegral (sum rightBucket) :: Double
+    ((sumLeftBucketDouble * giniLeft) + (sumRightBucketDouble * giniRight) / inputDataLenDouble)
+
+
+learnDecisionTree :: [([Double], String)] -> DecisionTree Int Double String 
+learnDecisionTree [] = EmptyTree
+learnDecisionTree (([], ""):rest) = EmptyTree
+learnDecisionTree inputData = do 
     let (leftBucket, rightBucket, classes) = initBuckets inputData 0
-    EmptyTree
+    if ((length(classes)) == 1)
+        then Leaf (classes !! 0)
+        else do
+            let (firstRow, firstClassName) = inputData !! 0
+            let attributesCount = length(firstRow)
+            let (bestGini, bestAttributeIndex, bestAttributeThreshold) = _learnDecisionTreeProcessAttribute inputData 0 attributesCount (leftBucket, rightBucket, classes) (1.0, -1, 0.0)
+            let (dataLeft, dataRight) = splitData inputData 0 (bestAttributeIndex, bestAttributeThreshold)
+        
+            Node bestAttributeIndex bestAttributeThreshold (learnDecisionTree dataLeft) (learnDecisionTree dataRight)
+
+
+_learnDecisionTreeProcessAttribute :: [([Double], String)] -> Int -> Int -> ([Int], [Int], [String]) -> (Double, Int, Double) -> (Double, Int, Double)
+_learnDecisionTreeProcessAttribute inputData processingAttributeIndex attributesCount (leftBucket, rightBucket, classes) (bestGini, bestAttributeIndex, bestAttributeThreshold)
+    | processingAttributeIndex >= attributesCount = (bestGini, bestAttributeIndex, bestAttributeThreshold)
+    | otherwise = do
+        let sortedTrainingData = sortBy (trainingDataSortFnc processingAttributeIndex) inputData
+        let (newBestGini, newBestAttributeIndex, newBestAttributeThreshold) = _learnDecisionTree sortedTrainingData 0 (leftBucket, rightBucket, classes) processingAttributeIndex (bestGini, bestAttributeIndex, bestAttributeThreshold)
+        if (newBestGini < bestGini)
+              then _learnDecisionTreeProcessAttribute inputData (processingAttributeIndex + 1) attributesCount (leftBucket, rightBucket, classes) (newBestGini, newBestAttributeIndex, newBestAttributeThreshold)
+              else _learnDecisionTreeProcessAttribute inputData (processingAttributeIndex + 1) attributesCount (leftBucket, rightBucket, classes) (bestGini, bestAttributeIndex, bestAttributeThreshold)
+
+
+_learnDecisionTree :: [([Double], String)] -> Int -> ([Int], [Int], [String]) -> Int -> (Double, Int, Double) -> (Double, Int, Double)
+_learnDecisionTree inputData processingIndex (leftBucket, rightBucket, classes) attributeIndex (bestGini, bestAttributeIndex, bestAttributeThreshold)
+    | (length(inputData)) <= processingIndex =  (bestGini, bestAttributeIndex, bestAttributeThreshold)
+    | processingIndex == 0 = do
+        let (row, className) = inputData !! processingIndex
+        let classIndex = findIndexOf classes className
+        let newLeftBucketValue = (leftBucket !! classIndex) + 1
+        let newLeftBucket = setValueAt leftBucket newLeftBucketValue classIndex
+
+        let newRightBucketValue = (rightBucket !! classIndex) - 1
+        let newRightBucket = setValueAt rightBucket newRightBucketValue classIndex
+
+        let giniLeft = giniScore newLeftBucket classIndex
+        let giniRight = giniScore newRightBucket classIndex
+        let newGini = calcGini (newLeftBucket, giniLeft) (newRightBucket, giniRight) (length(inputData))
+        if(newGini < bestGini)
+            then do
+            let attributeThreshold = ((row !! attributeIndex))
+            _learnDecisionTree inputData (processingIndex + 1) (leftBucket, rightBucket, classes) attributeIndex (newGini, attributeIndex, attributeThreshold)
+            else _learnDecisionTree inputData (processingIndex + 1) (leftBucket, rightBucket, classes) attributeIndex (bestGini, bestAttributeIndex, bestAttributeThreshold)
+    | otherwise = do 
+        let (row, className) = inputData !! processingIndex
+        let classIndex = findIndexOf classes className
+        let newLeftBucketValue = (leftBucket !! classIndex) + 1
+        let newLeftBucket = setValueAt leftBucket newLeftBucketValue classIndex
+
+        let newRightBucketValue = (rightBucket !! classIndex) - 1
+        let newRightBucket = setValueAt rightBucket newRightBucketValue classIndex
+
+        let giniLeft = giniScore newLeftBucket classIndex
+        let giniRight = giniScore newRightBucket classIndex
+        let newGini = calcGini (newLeftBucket, giniLeft) (newRightBucket, giniRight) (length(inputData))
+        let (prevRow, prevClassName) = inputData !! (processingIndex - 1)
+        if((row !! attributeIndex) == (prevRow !! attributeIndex))
+            then _learnDecisionTree inputData (processingIndex + 1) (leftBucket, rightBucket, classes) attributeIndex (bestGini, bestAttributeIndex, bestAttributeThreshold)
+            else do
+                if(newGini < bestGini)
+                    then do
+                    let attributeThreshold = ((row !! attributeIndex) + (prevRow !! attributeIndex)) / 2
+                    _learnDecisionTree inputData (processingIndex + 1) (leftBucket, rightBucket, classes) attributeIndex (newGini, attributeIndex, attributeThreshold)
+                    else _learnDecisionTree inputData (processingIndex + 1) (leftBucket, rightBucket, classes) attributeIndex (bestGini, bestAttributeIndex, bestAttributeThreshold)
+
+
+
+trainingDataSortFnc :: Int -> ([Double], String) -> ([Double], String) -> Ordering
+trainingDataSortFnc index1D (data1, className1) (data2, className2) = 
+    compare (data1 !! index1D) (data2 !! index1D)
 
 main :: IO ()
 main = do
@@ -268,7 +355,7 @@ main = do
     let grid = (convertToGridOfDoubles contents ',' '\n')
     printGrid grid
 
-    let result = decisionTree_clasifyAll tree grid 0
+    let result = decisionTree_clasifyAll tree grid 0;
 
     putStrLn (show result)
 
@@ -288,6 +375,9 @@ main = do
     print leftBucket
     print rightBucket
     print classes
+
+    let learnedDecisionTree = learnDecisionTree trainingData
+    printDecisionTree learnedDecisionTree
 
     let grid = ([4,5,6] : [[1, 2, 3]])
     printGrid grid
